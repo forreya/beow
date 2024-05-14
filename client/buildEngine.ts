8f9da3d275cc044ab11874de5c37ffab8533769d
Ryan Lai <ryanlai12@gmail.com>
2024-05-14 15:02:56 +0100

    Implemented file watcher into the build engine

--- client/buildEngine.ts
+++ client/buildEngine.ts
@@ -1,18 +1,45 @@
+import fs from "fs";
+
 const debounce = <T extends unknown[]>(funcToDebounce: (...args: T) => void) => {
-    let timer: ReturnType<typeof setTimeout> | undefined = undefined
-    return (...args: T) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            funcToDebounce(...args);
-        }, 50);
-    }
+	let timer: ReturnType<typeof setTimeout> | undefined = undefined
+	return (...args: T) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			funcToDebounce(...args);
+		}, 50);
+	}
 }
 
-const func = () => {
-    console.log('I have been debounced');
-}
+const triggerExtensions = /^.*\.(tsx?|jsx?|png|jpg|webp|svg|css)$/;
 
-const debouncedFunc = debounce(func);
-debouncedFunc();
-debouncedFunc();
-setTimeout(debouncedFunc, 2000)
+const fileWatcher = fs.watch(
+	import.meta.dir + "/../src",
+	{
+		recursive: true
+	},
+	debounce((_, filename) => {
+		if (typeof filename !== "string") {
+			return;
+		}
+		if (!triggerExtensions.exec(filename)) {
+			return;
+		}
+		console.log("Firing up build engine...");
+		// Run build script here
+		console.log("Rebuild completed.")
+	})
+);
+
+process.on("SIGINT", () => {
+	try {
+		console.log("\nShutting off build engine...");
+		fileWatcher.close();
+	} catch (error) {
+		console.error("\n" + error);
+	}
+	process.exit(0);
+})
+
+console.log("Firing up build engine...");
+// Run build script here
+console.log("Rebuild completed.")
